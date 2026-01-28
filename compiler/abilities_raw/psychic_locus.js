0dea34e26d0b860728b01d970d432c0910214757
csizmadiadi <adicsizmadi@gmail.com>
2026-01-28 22:45:15 +0100

    new abilities added: Thunderous Territory, Psychic Locus Doomed Divison now has flavor text, the Effect tag and stats now correctly say its Damage Type (Necrotic)

--- compiler/abilities_raw/psychic_locus.js
+++ compiler/abilities_raw/psychic_locus.js
@@ -1,18 +1,18 @@
 module.exports = {
-    name: "Doomed Divison",
+    name: "Psychic Locus",
     category: "stance",
     type: [
         "Spell",
     ],
     reqs_short: [
-        "10 CS, (5 Anima | 10 Abjuration | 20 Enchantment)",
+        "5 Anima | 10 Abjuration | 20 Enchantment",
     ],
     reqs_full: [
         "<b>Spell:</b> 10 CS, (5 Anima | 10 Abjuration | 20 Enchantment)",
     ],
     tags: [
         "Offense",
-        "Ranged, Static Sphere AOE, Necrotic Damage, VIT Save, Debuff, Effect",
+        "Ranged, Static Sphere AOE, Psychic Damage, VIT Save, Effect",
         "Main-Action / Continuous",
     ],
     unlock_cost: 10,
@@ -20,24 +20,25 @@ module.exports = {
         {
             type: "p",
             content: [
-                `Within Range, in a choosen Sphere secrade light starts to emanate.`,
-                `When using this Ability, you are put in <b>Doomed Divison Stance</b>.`,
-                `When Entering this Stance and at the Start of your subsequent Turns, Entities in the Radius must Succeed a <b>VIT Save</b> or take Damage.`,
+                `Within Range, in a choosen Sphere the air becomes dusty, and small rocks will start to from.`,
+                `When using this Ability, you are put in <b>Psychic Locus Stance</b>.`,
+                `When Entering this Stance and at the Start of your subsequent Turns, Entities in the Radius must Succeed a <b>Soul Save</b> or take Damage.`,
             ],
         },
         {
             type: "list",
             content: [
                 `<b>Overflow:</b>`,
-                `As a <b>Main-Action</b>, you can now inflict Damaged Entities with the <b>Darkness Effect</b>.`,
+                `As a <b>Main-Action</b>, you can now inflict Damaged Entities with the <b>Charmed Effect</b>.`,
                 `This can be done only once on this Ability.`,
             ],
         },
-        {
+                {
             type: "list",
             content: [
-                `<b>Darkness Effect:</b>`,
-                `All Healing, Shielding, and BUFFER they recieve until the End of their Next Turn is Reduced by <box>6 × ESS</box>`,
+                `<b>Charmed Effect:</b>`,
+                `The Charmed Target makes <b>Soul Save's</b> with Disadvantage.`,
+                `This Effect Ends at the End of Casters Next Turn.`,
             ],
         },
         {
@@ -58,9 +59,8 @@ module.exports = {
                 ["Stance Trigger", "Continuous"],
                 ["Range", "5 × CS^(1/3)"],
                 ["Radius", "CS^(1/3)"],
-                ["VIT Save", "8 × ESS"],
-                ["Damage", "(1d10 × ESS) Necrotic"],
-                ["Healing Reductions", "6 × ESS"],
+                ["Soul Save", "8 × ESS"],
+                ["Damage", "(2d6 × ESS) Psychic"],
             ],
         },
     ],
--- compiler/abilities_raw/thunderous_territory.js
+++ compiler/abilities_raw/thunderous_territory.js
@@ -1,18 +1,18 @@
 module.exports = {
-    name: "Doomed Divison",
+    name: "Thunderous Territory",
     category: "stance",
     type: [
         "Spell",
     ],
     reqs_short: [
-        "10 CS, (5 Anima | 10 Abjuration | 20 Enchantment)",
+        "5 Evocation | 10 Transmutation | 20 Enchantment",
     ],
     reqs_full: [
-        "<b>Spell:</b> 10 CS, (5 Anima | 10 Abjuration | 20 Enchantment)",
+        "<b>Spell:</b> 10 CS, (5 Evocation | 10 Transmutation | 20 Enchantment)",
     ],
     tags: [
         "Offense",
-        "Ranged, Static Sphere AOE, Necrotic Damage, VIT Save, Debuff, Effect",
+        "Ranged, Static Sphere AOE, Thunder Damage, VIT Save",
         "Main-Action / Continuous",
     ],
     unlock_cost: 10,
@@ -20,8 +20,8 @@ module.exports = {
         {
             type: "p",
             content: [
-                `Within Range, in a choosen Sphere secrade light starts to emanate.`,
-                `When using this Ability, you are put in <b>Doomed Divison Stance</b>.`,
+                `Within Range, in a choosen Sphere the air becomes dusty, and small rocks will start to from.`,
+                `When using this Ability, you are put in <b>Thunderous Territory Stance</b>.`,
                 `When Entering this Stance and at the Start of your subsequent Turns, Entities in the Radius must Succeed a <b>VIT Save</b> or take Damage.`,
             ],
         },
@@ -29,15 +29,8 @@ module.exports = {
             type: "list",
             content: [
                 `<b>Overflow:</b>`,
-                `As a <b>Main-Action</b>, you can now inflict Damaged Entities with the <b>Darkness Effect</b>.`,
+                `As a <b>Main-Action</b>, and at the Start of your subsequent Turns, you can reposition this Ability within Range.`,
                 `This can be done only once on this Ability.`,
-            ],
-        },
-        {
-            type: "list",
-            content: [
-                `<b>Darkness Effect:</b>`,
-                `All Healing, Shielding, and BUFFER they recieve until the End of their Next Turn is Reduced by <box>6 × ESS</box>`,
             ],
         },
         {
@@ -57,10 +50,9 @@ module.exports = {
                 ["Cast Time", "Main-Action"],
                 ["Stance Trigger", "Continuous"],
                 ["Range", "5 × CS^(1/3)"],
-                ["Radius", "CS^(1/3)"],
+                ["Radius", "2 × CS^(1/3)"],
                 ["VIT Save", "8 × ESS"],
-                ["Damage", "(1d10 × ESS) Necrotic"],
-                ["Healing Reductions", "6 × ESS"],
+                ["Damage", "(2d4 × ESS) Thunder"],
             ],
         },
     ],
