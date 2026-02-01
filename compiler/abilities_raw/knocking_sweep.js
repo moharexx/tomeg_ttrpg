eea343e6bfb56ec26e90e8387ecef07adf2b602a
csizmadiadi <adicsizmadi@gmail.com>
2026-02-01 20:22:37 +0100

    small changes Knocking Sweep got reworked, overall buff and more utility Digital Dice Roller minor programing changes (it was dog shit its less so now)

--- compiler/abilities_raw/knocking_sweep.js
+++ compiler/abilities_raw/knocking_sweep.js
@@ -8,7 +8,7 @@ module.exports = {
         "Melee Weapon | Martial Arts",
     ],
     reqs_full: [
-        "<b>Skill:<b> 10 Melee Weapon | 10 Martial Arts",
+        "<b>Skill:</b> 10 Melee Weapon | 20 Martial Arts",
     ],
     tags: [
         "Defense",
@@ -20,16 +20,15 @@ module.exports = {
         {
             type: "p",
             content: [
-                "When using this Ability, you attempt to knock Entities away from yourself.",
-                "Entities inside your Melee Reach must succeed a <b>STR Save</b> of <box>6 × ESS</box> or be Knockbacked.",
-                "The Direction of the Knockback is directly aways from you.",
+                `When using this Ability, you attempt to knock Entities away from yourself.`,
+                `Entities inside your Melee Reach must succeed a <b>STR Save</b> of <box>10 × ESS</box> or be Knockbacked.`,
+                `The Direction of the Knockback is directly aways from you.`,
             ],
         },
         {
             type: "dots",
             content: [
-                "You can Spend GRIT to increase the <b>STR Save</b>, the Bonus Save is equal to GRIT spent.",
-                "<b>Maximum GRIT Spending:</b> <box>6 × ESS</box>",
+                `You can Spend <box>5 × ESS</box> GRIT to change the Knockback direction for each Entity.`,
             ],
         },
     ],
@@ -42,7 +41,7 @@ module.exports = {
                 ["Ability Stat", "Value"],
                 ["Cast Time", "Main-Action"],
                 ["Range", "Reach"],
-                ["STR Save", "6 × ESS"],
+                ["STR Save", "10 × ESS"],
                 ["Knockback", "2 × ESS^(1/3)"],
             ],
         },
